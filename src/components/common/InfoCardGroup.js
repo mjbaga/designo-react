67861b57bd42081ab0da1737396f3aa277cdd759
mjbaga <mjdbaga@gmail.com>
2022-02-28 16:05:25 +0800

    Refactor card group to reduce components

--- src/components/common/InfoCardGroup.js
+++ src/components/common/InfoCardGroup.js
@@ -1,11 +1,18 @@
-import InfoCard from 'components/common/InfoCard';
 import styles from './InfoCardGroup.module.scss';
 
 const InfoCardGroup = ({ items }) => {
 
   return (
     <div className={styles["info-card-group"]}>
-      {items.map((item, i) => (<InfoCard key={i} item={item} />))}
+      {items.map((item, i) => (
+        <div className={styles["info-card"]} key={i}>
+          <div className={styles["image-container"]}>
+            <img src={item.image} alt={item.title} />
+          </div>
+          <h3>{item.title}</h3>
+          <p>{item.desc}</p>
+        </div>
+      ))}
     </div>
   )
 }
