65d6176138df408f3871d2852b3499a3fcb1dfdb
mjbaga <mjdbaga@gmail.com>
2022-03-09 14:36:02 +0800

    Implement lazyload import function

--- src/App.js
+++ src/App.js
@@ -1,61 +1,18 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from 'pages/Homepage';
 import LoadingSpinner from 'components/layout/LoadingSpinner';
-// import { lazyDelayed } from 'utils/CustomFunctions';
+import { lazyDelayed } from 'utils/CustomFunctions';
 
 import './App.scss';
 
-// Importing one by one cos can't make lazyDelayed function work
-const WebDesign = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/WebDesign')), 1000);
-  });
-});
-
-const AppDesign = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/AppDesign')), 1000);
-  });
-});
-
-const GraphicDesign = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/GraphicDesign')), 1000);
-  });
-});
-
-const About = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/About')), 1000);
-  });
-});
-
-const Locations = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/Locations')), 1000);
-  });
-});
-
-const Contact = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/Contact')), 1000);
-  });
-});
-
-const DesignSystem = lazy(() => {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(import('pages/DesignSystem')), 1000);
-  });
-});
-
-// const WebDesign = lazy(() => import('pages/WebDesign'));
-// const AppDesign = lazy(() => import('pages/AppDesign'));
-// const GraphicDesign = lazy(() => import('pages/GraphicDesign'));
-// const About = lazy(() => import('pages/About'));
-// const Locations = lazy(() => import('pages/Locations'));
-// const Contact = lazy(() => import('pages/Contact'));
-// const DesignSystem = lazy(() => import('pages/DesignSystem'));
+const WebDesign = lazyDelayed(import('pages/WebDesign'));
+const AppDesign = lazyDelayed(import('pages/AppDesign'));
+const GraphicDesign = lazyDelayed(import('pages/GraphicDesign'));
+const About = lazyDelayed(import('pages/About'));
+const Locations = lazyDelayed(import('pages/Locations'));
+const Contact = lazyDelayed(import('pages/Contact'));
+const DesignSystem = lazyDelayed(import('pages/DesignSystem'));
 
 function App() {
 
